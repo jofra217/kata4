d906de7658216605a4fdb40f47524350c8f12999
joframontesdeocanuez <joframontesdeocanuez@10.230.169.178>
2016-10-28 09:04:59 +0100

    Kata4: versión 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -5,11 +5,19 @@ import java.io.File;
 
 public class Kata4 {
     public static void main(String[] args) {
-        File file = new File("/Users/joframontesdeocanuez/");
-        String[] names = file.list();
-        for (String name : names) {
-            System.out.println(name);
-        }
+        File file = new File("/Users/joframontesdeocanuez/downloads");
+        print(file.listFiles(), "");
     }
+    
+    private static void print(File[] files, String indent){
+        if (files == null) return;
+        for (File file : files) {
+            System.out.println(indent + (file.isDirectory() ? "+" : "-")
+                    + file.getName());
+            if (!file.isDirectory() || file.isHidden()) continue;
+            print (file.listFiles(), "    ");
+        }
+        
+    };
 
 }
